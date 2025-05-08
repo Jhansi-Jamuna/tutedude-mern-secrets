d9ff12f88c8362efbbaa6b808dc8dfacdfcb9464
jhansi <jhansijamuna23@gamail.com>
2025-05-08 21:35:06 +0530

    Updated app.js to connect database

--- SECRETS/app.js
+++ SECRETS/app.js
@@ -1,40 +1,51 @@
-const express=require('express')
-const bodyParser= require('body-parser');
-const encrypt= require('mongoose-encryption');
+const express = require('express');
+const bodyParser = require('body-parser');
+const encrypt = require('mongoose-encryption');
+const mongoose = require("mongoose");
 
+const app = express();
 
-var app=express();
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://jamunapawar12:jhansi2313@secondsdb.nfyn5ad.mongodb.net/?retryWrites=true&w=majority&appName=secondsdb");
-const trySchema=new mongoose.Schema({
-    email:String,
-    password:String
+// Connect to the correct MongoDB database
+mongoose.connect("mongodb+srv://jamunapawar12:jhansi2313@secondsdb.nfyn5ad.mongodb.net/userDB?retryWrites=true&w=majority&appName=secondsdb");
+
+// Define the schema
+const trySchema = new mongoose.Schema({
+    email: String,
+    password: String
 });
-const secret="thisislittlesecret.";
-trySchema.plugin(encrypt, {secret:secret, encryptedFields:["password"] })
 
-const item=mongoose.model("second", trySchema);
+// Apply encryption plugin
+const secret = "thisislittlesecret.";
+trySchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
-app.get("/", function(req,res){
+// Use 'User' to map to 'users' collection
+const User = mongoose.model("User", trySchema);
+
+// Routes
+app.get("/", function (req, res) {
     res.render("home");
-})
+});
 
-app.get("/login", function(req,res){
+app.get("/login", function (req, res) {
     res.render("login");
-})
+});
 
-app.post("/register", async function(req, res) {
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", async function (req, res) {
     try {
-        const newUser = new item({
+        const newUser = new User({
             email: req.body.username,
             password: req.body.password
         });
 
-        await newUser.save(); // no callback here
+        await newUser.save();
         res.render("secrets");
     } catch (err) {
         console.error(err);
@@ -42,16 +53,16 @@ app.post("/register", async function(req, res) {
     }
 });
 
-app.post("/login", async function(req, res) {
+app.post("/login", async function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
     try {
-        const foundUser = await item.findOne({ email: username });
+        const foundUser = await User.findOne({ email: username });
 
         if (foundUser) {
             if (foundUser.password === password) {
-                res.render("secrets"); // This will render secrets.ejs
+                res.render("secrets");
             } else {
                 res.send("Incorrect password");
             }
@@ -64,11 +75,7 @@ app.post("/login", async function(req, res) {
     }
 });
 
-app.get("/register", function(req,res){
-    res.render("register");
-})
-
-
-app.listen(5000, function(){
-    console.log("Server started");
-})
+// Start the server
+app.listen(5000, function () {
+    console.log("Server started on port 5000");
+});
