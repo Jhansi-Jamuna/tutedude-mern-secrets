e5de8764b5b11efbff1dfbf162631cdd73334c02
jhansi <jhansijamuna23@gamail.com>
2025-05-09 00:19:10 +0530

    edit functionality added

--- SECRETS/app.js
+++ SECRETS/app.js
@@ -83,7 +83,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("./models/userSchema");
 const app = express();
 const PORT = process.env.PORT || 5000;
